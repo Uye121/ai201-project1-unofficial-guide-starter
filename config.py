db1d0856413f2d6f21deeabfb72cab72cb5880ef
Ulric Ye <yye90012@gmail.com>
2026-06-07 16:30:47 -0700

    Update chroma collection name.

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ LLM_MODEL = "llama-3.3-70b-versatile"
 EMBEDDING_MODEL = "all-MiniLM-L6-v2"
 
 # --- Vector store ---
-CHROMA_COLLECTION = "rulesbot"
+CHROMA_COLLECTION = "UCSDSocialBot"
 CHROMA_PATH = "./chroma_db"
 
 # --- Retrieval ---
